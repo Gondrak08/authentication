c824982f12152e9fe09dafe8fc3a37ec9bc4a338
Gondrak08 <vitor.alecrim.p@gmail.com>
2020-05-18 17:24:22 -0300

    authentication component 1.0.0

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View,StatusBar } from 'react-native';
+
+
+import Routes from './src/routes'
+
+export default function App() {
+  return (
+    <>
+      <StatusBar hidden = {true} />
+      <Routes/>
+
+   
+
+    </>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
